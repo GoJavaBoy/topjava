a3f04736d6634d9a884b0b219e8def376effd4b8
Stasis Yukniavichus <stasys1996@gmail.com>
2021-03-07 11:24:58 +0000

    HW05_1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,7 +1,9 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -9,35 +11,49 @@ import java.util.List;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
+    private static final Sort SORT_DATE = Sort.by(Sort.Direction.DESC, "dateTime");
 
     private final CrudMealRepository crudRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    private final CrudUserRepository crudUserRepository;
+
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
         this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        meal.setUser(crudUserRepository.getOne(userId));
+        if (meal.isNew()) {
+            return crudRepository.save(meal);
+        } else if (get(meal.id(), userId) == null){
+            return null;
+        }
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        Meal entity = get(id, userId);
+        return entity != null && crudRepository.delete(id) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = crudRepository.findById(id).orElse(null);
+        return meal != null && meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        User user = crudUserRepository.getOne(userId);
+        return crudRepository.findAllByUser(user, SORT_DATE);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        User user = crudUserRepository.getOne(userId);
+        return crudRepository.findAllByUserAndDateTimeGreaterThanEqualAndDateTimeLessThan(user, startDateTime, endDateTime, SORT_DATE);
     }
 }
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,14 +1,12 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.web.meal.MealRestController;
-import ru.javawebinar.topjava.web.user.AdminRestController;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.datajpa.DataJpaMealRepository;
+import ru.javawebinar.topjava.repository.jpa.JpaMealRepository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
@@ -17,20 +15,43 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
+        try (ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext()) {
+            appCtx.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile());
+            appCtx.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+            appCtx.refresh();
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-            System.out.println();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetween(
-                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExcess.forEach(System.out::println);
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+//            System.out.println();
+//
+//            MealRestController mealController = appCtx.getBean(MealRestController.class);
+//            List<MealTo> filteredMealsWithExcess =
+//                    mealController.getBetween(
+//                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
+//                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
+//            filteredMealsWithExcess.forEach(System.out::println);
+//            System.out.println();
+//            System.out.println(mealController.getBetween(null, null, null, null));
+
+            DataJpaMealRepository mealRepository = appCtx.getBean(DataJpaMealRepository.class, "dataJpaMealRepository");
+            JpaMealRepository jpaMealRepository = appCtx.getBean(JpaMealRepository.class);
+            List<Meal> dataMealList = mealRepository.getBetweenHalfOpen(
+                    LocalDateTime.of(LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0)),
+                    LocalDateTime.of(LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0)),
+                    100000
+                    );
+            List<Meal> jpaMealList = jpaMealRepository.getBetweenHalfOpen(
+                    LocalDateTime.of(LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0)),
+                    LocalDateTime.of(LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0)),
+                    100000
+            );
+            dataMealList.forEach(meal -> System.out.println(meal.toString()));
             System.out.println();
-            System.out.println(mealController.getBetween(null, null, null, null));
+            System.out.println();
+            System.out.println();
+            jpaMealList.forEach(meal -> System.out.println(meal.toString()));
+
         }
     }
 }
